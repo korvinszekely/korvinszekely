836af2af22319fb75d496da2b04e42ec9cc728ad
NOR0i <sarah.ma@torontomu.ca>
2025-05-20 19:32:08 -0400

    Update loadImagesFrom.js

--- src/components/loadImagesFrom.js
+++ src/components/loadImagesFrom.js
@@ -1,9 +1,9 @@
 // src/utils/loadImages.js
 
 const contexts = {
-  photography: require.context('../images/photography/', false, /\.(png|jpe?g|svg)$/),
-  artwork: require.context('../images/artwork/', false, /\.(png|jpe?g|svg)$/),
-  music: require.context('../images/music/', false, /\.(png|jpe?g|svg)$/),
+  photography: require.context('../images/photography/', false, /\.(png|jpe?g|svg|JPG|PNG|JPEG)$/),
+  artwork: require.context('../images/artwork/', false, /\.(png|jpe?g|svg|JPG|PNG|JPEG)$/),
+  music: require.context('../images/music/', false, /\.(png|jpe?g|svg|JPG|PNG|JPEG)$/),
 };
 
 export function loadImagesFrom(folder) {
